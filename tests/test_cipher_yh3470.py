5223045593f6067ac6e7dab305934d8fb224acfe
AileenHuang <113069041+AileenHuang@users.noreply.github.com>
2022-10-30 14:45:57 -0400

    2

--- tests/test_cipher_yh3470.py
+++ tests/test_cipher_yh3470.py
@@ -1,1 +1,9 @@
-from cipher_yh3470 import cipher_yh3470
+from cipher_Yazhao_Huang import cipher_yh3470
+import pytest
+
+def test_cipher_a():
+    example = 'Lisa'
+    expected = 'Mjtb'
+    actual = cipher(example, 1)
+    assert actual == expected
+    
